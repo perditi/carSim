eb66f4d070034582416fcb0f9658acd2f6c66f77
perditi <104952236+perditi@users.noreply.github.com>
2023-06-02 15:02:00 -0400

    update
    made cars comparable based on arrival time, quality of life stuff. fixed the time thing so time.sleep() actually changes the arrival time of the car by using time.localtime() instead of whatever I used before

--- someContribution.py
+++ someContribution.py
@@ -13,35 +13,66 @@ DIRECTIONS = np.array(["left", "straight", "right"])
 GEN = np.random.default_rng(None)#use GEN.poisson(1)
 #lambda is average number of cars per period of time -- how long do i want??
 #five seconds isn't bad but how fast are these fucking cars going :skull:
-#returns array of 4 numbers, (N E S W) because it's a 4-way intersection
 
-PER_SECONDS = 30#say the period of time is 30 seconds
+PER_SECONDS = 5#say the period of time is 30 seconds
 
 class Car:
     def __init__(self):
         self.direction = DIRECTIONS[GEN.integers(0,3)]#random direction
-        self.arrivalTime = time.process_time_ns()
+        self.arrivalTime = time.localtime()
+    def __eq__(self, other):
+        return (self.arrivalTime == other.arrivalTime)
+    def __ne__(self, other):
+        return not (self == other)
+    def __lt__(self, other):
+        return (self.arrivalTime < other.arrivalTime)
+    def __le__(self, other):
+        return ((self == other) or (self < other))
+    def __gt__(self, other):
+        return not (self < other)
+    def __ge__(self, other):
+        return ((self == other) or (not (self < other)))
     def __str__(self):
-        return f"Turning {self.direction}, arrived at {self.arrivalTime}."
-
+        return f"Turning {self.direction}, arrived at " + time.strftime("%H:%M:%S", self.arrivalTime)
+    
 class Road:
     def __init__(self):
         self.q = q.Queue()
+        
     def put(self, n):
         for i in range(n):
             self.q.put(Car())
-            time.sleep(PER_SECONDS/n)
+            if i < n:
+                time.sleep(PER_SECONDS/(n-1))
+            
     def pop(self):
-        self.q.pop()
+        return self.q.get()
+    
     def __str__(self):
-        for i in self.q:
-            return None
+        tempQ = q.Queue()
+        string = ""
+        r = self.q.qsize()
+        for i in range(r):
+            temp = self.q.get()
+            string = string + temp.__str__() + "\n"
+            tempQ.put(temp)
+        for i in range(r):
+            self.q.put(tempQ.get())
+        return string
+        
             
 
 ROADS = np.array([Road(), Road(), Road(), Road()])#N E S W
 
-cars = np.array([None, None], dtype=Car)
-cars[0] = Car()
-cars[1] = Car()
-print(cars[0])
-print(cars[1])
+
+ROADS[0].put(2)
+print(ROADS[0])
+test1 = ROADS[0].pop()
+test2 = ROADS[0].pop()
+print(test1)
+print(test2)
+print(test1 < test2)
+print(test2 > test1)
+print(test1 != test2)
+
+
