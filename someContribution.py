8518bea0a6aa2dfce6c41d18401293bbb395d2ad
perditi <104952236+perditi@users.noreply.github.com>
2023-06-02 13:02:54 -0400

    new update
    cars have times now so the order is determined by which car gets to the intersection first
    I just did the dishes now I gotta eat something and head to campus

--- someContribution.py
+++ someContribution.py
@@ -7,15 +7,22 @@ Created on Fri Jun  2 10:37:01 2023
 
 import numpy as np
 import queue as q
+import time
 
 DIRECTIONS = np.array(["left", "straight", "right"])
+GEN = np.random.default_rng(None)#use GEN.poisson(1)
+#lambda is average number of cars per period of time -- how long do i want??
+#five seconds isn't bad but how fast are these fucking cars going :skull:
+#returns array of 4 numbers, (N E S W) because it's a 4-way intersection
+
+PER_SECONDS = 30#say the period of time is 30 seconds
 
 class Car:
     def __init__(self):
-        
-        self.direction = DIRECTIONS[0]
-        print(self.direction)
-
+        self.direction = DIRECTIONS[GEN.integers(0,3)]#random direction
+        self.arrivalTime = time.process_time_ns()
+    def __str__(self):
+        return f"Turning {self.direction}, arrived at {self.arrivalTime}."
 
 class Road:
     def __init__(self):
@@ -23,11 +30,18 @@ class Road:
     def put(self, n):
         for i in range(n):
             self.q.put(Car())
+            time.sleep(PER_SECONDS/n)
+    def pop(self):
+        self.q.pop()
+    def __str__(self):
+        for i in self.q:
+            return None
+            
 
+ROADS = np.array([Road(), Road(), Road(), Road()])#N E S W
 
-gen = np.random.default_rng(None)#use gen.poisson(1)
-#lambda is average number of cars per period of time -- how long do i want?? rn set to 1
-#five seconds isn't bad but how fast are these fucking cars going :skull:
-#returns array of 4 numbers, (N E S W) because it's a 4-way intersection
-
-
+cars = np.array([None, None], dtype=Car)
+cars[0] = Car()
+cars[1] = Car()
+print(cars[0])
+print(cars[1])
